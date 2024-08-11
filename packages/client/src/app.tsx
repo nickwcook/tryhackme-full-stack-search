96a37b3ac7a7a7fe526d86d7c8c0d311d6838114
nickwcook <nickwcook@hotmail.com>
2024-08-11 14:32:00 +0100

    feat: render country search results

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,5 +1,9 @@
 import { useState, type ChangeEvent } from 'react';
-import { fetchAndFilterHotels } from "./utils/search.utils";
+import {
+  fetchAndFilterCities,
+  fetchAndFilterCountries,
+  fetchAndFilterHotels
+} from "./utils/search.utils";
 import Hotel from "./types/Hotel.type";
 import City from "./types/City.type";
 import Country from "./types/Country.type";
@@ -12,10 +16,10 @@ import Country from "./types/Country.type";
 function App() {
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [showClearBtn, setShowClearBtn] = useState<boolean>(false);
-  // TODO: Combine state values
+  // TODO: Combine state values into 'searchResults'
   const [hotels, setHotels] = useState<Hotel[]>([]);
-  const [cities, setCities] = useState<City[]>([]);
   const [countries, setCountries] = useState<Country[]>([]);
+  const [cities, setCities] = useState<City[]>([]);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
@@ -29,9 +33,14 @@ function App() {
       return;
     }
 
+    // TODO: Combine logic into single function?
     const filteredHotels = await fetchAndFilterHotels(searchTerm);
+    const filteredCountries = await fetchAndFilterCountries(searchTerm);
+    const filteredCities = await fetchAndFilterCities(searchTerm);
     setShowClearBtn(true);
     setHotels(filteredHotels);
+    setCountries(filteredCountries);
+    setCities(filteredCities);
   };
 
   const resetSearchResults = () => {
@@ -45,8 +54,19 @@ function App() {
     resetSearchResults();
   }
 
-  const renderHotelsList = () => (
-    <div className="search-dropdown-menu dropdown-menu w-100 show p-2">
+  const renderSearchResults = () => (
+    !!(hotels.length || cities.length || countries.length) && (
+      <div className="search-dropdown-menu dropdown-menu w-100 show p-2">
+        {renderHotelResults()}
+        {renderCountryResults()}
+        <h2>Cities</h2>
+        <p>No cities matched</p>
+      </div>
+    )
+  );
+
+  const renderHotelResults = () => (
+    <>
       <h2>Hotels</h2>
       {hotels.length ? hotels.map((hotel, index) => (
         <li key={index}>
@@ -54,14 +74,28 @@ function App() {
             <i className="fa fa-building mr-2"></i>
             {hotel.hotel_name}
           </a>
-          <hr className="divider" />
+          <hr className="divider"/>
         </li>
       )) : <p>No hotels matched</p>}
+    </>
+  );
+
+  const renderCountryResults = () => (
+    <>
       <h2>Countries</h2>
-      <p>No countries matched</p>
-      <h2>Cities</h2>
-      <p>No cities matched</p>
-    </div>
+      {countries.length ? countries.map((country, index) => {
+        const { country: countryName } = country; // Note observation re. field 'country'
+        return (
+          <li key={index}>
+            <a href={`/hotels/${country._id}`} className="dropdown-item">
+              <i className="fa fa-building mr-2"></i>
+              {countryName}
+            </a>
+            <hr className="divider"/>
+          </li>
+        )
+      }) : <p>No countries matched</p>}
+    </>
   );
 
   return (
@@ -85,7 +119,7 @@ function App() {
                   </span>
                 )}
               </div>
-              {!!hotels.length && renderHotelsList()}
+              {renderSearchResults()}
             </div>
           </div>
         </div>
