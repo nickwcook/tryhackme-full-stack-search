e4ab40455f403fa668da1cac20175c3af566b20a
nickwcook <nickwcook@hotmail.com>
2024-08-13 15:37:40 +0100

    test: test hide of clear button and clear of input on click of clear button

--- packages/client/src/app.test.tsx
+++ packages/client/src/app.test.tsx
@@ -1,6 +1,6 @@
-import { describe, it, expect, beforeAll, vi } from "vitest";
+import { describe, it, expect, beforeAll, beforeEach, vi } from "vitest";
 import { render, screen, act, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import userEvent, { UserEvent } from "@testing-library/user-event";
 import App from "app.tsx";
 import * as searchUtils from "utils/search.utils";
 
@@ -32,10 +32,16 @@ describe("App component", () => {
   });
 
   describe("user enters multi-character search term", () => {
-    it("initiates single fetch request for each of hotels/countries/cities after a 500ms debounce", async () => {
-      const user = userEvent.setup();
+    let user: UserEvent;
+    let input: HTMLInputElement;
+
+    beforeEach(async () => {
+      user = userEvent.setup();
       await act(async () => render(<App />));
-      const input = screen.getByRole("textbox");
+      input = screen.getByRole("textbox");
+    });
+
+    it("initiates single fetch request for each of hotels/countries/cities after a 500ms debounce", async () => {
       await user.type(input, "a");
       await waitFor(
         () => {
@@ -66,12 +72,30 @@ describe("App component", () => {
     });
 
     it("shows the clear button", async () => {
-      const user = userEvent.setup();
-      await act(async () => render(<App />));
-      const input = screen.getByRole("textbox");
       await user.type(input, "a");
       const clearButton = screen.getByTitle("Clear search");
       expect(clearButton).toBeInTheDocument();
+    });
+  });
+
+  describe("when user clicks the clear button", () => {
+    let user: UserEvent;
+    let input: HTMLInputElement;
+    let clearButton: HTMLElement | null;
+
+    beforeEach(async () => {
+      user = userEvent.setup();
+      await act(async () => render(<App />));
+      input = screen.getByRole("textbox");
+      await user.type(input, "a");
+    });
+
+    it("hides the clear button and clears the input", async () => {
+      clearButton = screen.getByTitle("Clear search");
+      await user.click(clearButton);
+      clearButton = screen.queryByTitle("Clear search");
+      expect(clearButton).not.toBeInTheDocument();
+      expect(input.value).toBe("");
     });
   });
 });
