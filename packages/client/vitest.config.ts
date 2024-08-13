068afcf98ff3eb4ce5583b11ae3e3e51f3c3305e
nickwcook <nickwcook@hotmail.com>
2024-08-13 12:18:52 +0100

    chore: update vitest config to enable absolute imports via vite-tsconfig-paths

--- packages/client/vitest.config.ts
+++ packages/client/vitest.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
-
 import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   test: {
     environment: "jsdom",
     setupFiles: ["./vitest.setup.ts"],
   },
+  plugins: [tsConfigPaths()],
 });
