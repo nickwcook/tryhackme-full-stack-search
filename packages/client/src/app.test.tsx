d5e91cab2a64265b10618816f96c37c041abc8da
nickwcook <nickwcook@hotmail.com>
2024-08-13 15:20:57 +0100

    test: add tests for App component debounced search on user input

--- packages/client/src/app.test.tsx
+++ packages/client/src/app.test.tsx
@@ -1,9 +1,62 @@
-import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import App from "app";
+import { describe, it, expect, beforeAll, vi } from "vitest";
+import { render, screen, act, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "app.tsx";
+import * as searchUtils from "utils/search.utils";
 
-test("renders search input", () => {
-  render(<App />);
-  const input = screen.getByPlaceholderText("Search accommodation...");
-  expect(input).toBeInTheDocument();
+describe("App component", () => {
+  beforeAll(() => {
+    vi.useFakeTimers({ shouldAdvanceTime: true });
+    vi.spyOn(searchUtils, "fetchAndFilterHotels");
+    vi.spyOn(searchUtils, "fetchAndFilterCountries");
+    vi.spyOn(searchUtils, "fetchAndFilterCities");
+    vi.spyOn(AbortController.prototype, "abort").mockImplementation(vi.fn);
+  });
+
+  describe("when no search term", () => {
+    it("does not render any hotels, countries or cities", async () => {
+      await act(async () => render(<App />));
+      const hotelsText = screen.queryByText("Hotels");
+      const countriesText = screen.queryByText("Countries");
+      const citiesText = screen.queryByText("Cities");
+      expect(hotelsText).not.toBeInTheDocument();
+      expect(countriesText).not.toBeInTheDocument();
+      expect(citiesText).not.toBeInTheDocument();
+    });
+  });
+
+  describe("user enters multi-character search term", () => {
+    it("initiates single fetch request for each of hotels/countries/cities after a 500ms debounce", async () => {
+      const user = userEvent.setup();
+      await act(async () => render(<App />));
+      const input = screen.getByRole("textbox");
+      await user.type(input, "a");
+      await waitFor(
+        () => {
+          expect(searchUtils.fetchAndFilterHotels).toHaveBeenCalledTimes(0);
+          expect(searchUtils.fetchAndFilterCountries).toHaveBeenCalledTimes(0);
+          expect(searchUtils.fetchAndFilterCities).toHaveBeenCalledTimes(0);
+        },
+        { timeout: 400 },
+      );
+      await user.type(input, "b");
+      await waitFor(
+        () => {
+          expect(searchUtils.fetchAndFilterHotels).toHaveBeenCalledTimes(0);
+          expect(searchUtils.fetchAndFilterCountries).toHaveBeenCalledTimes(0);
+          expect(searchUtils.fetchAndFilterCities).toHaveBeenCalledTimes(0);
+        },
+        { timeout: 500 },
+      );
+      await user.type(input, "c");
+      await waitFor(
+        () => {
+          expect(searchUtils.fetchAndFilterHotels).toHaveBeenCalledTimes(1);
+          expect(searchUtils.fetchAndFilterCountries).toHaveBeenCalledTimes(1);
+          expect(searchUtils.fetchAndFilterCities).toHaveBeenCalledTimes(1);
+        },
+        { timeout: 501 },
+      );
+    });
+  });
 });
